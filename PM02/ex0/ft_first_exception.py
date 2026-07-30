1ddf6d196c2e898180fcac4e5406082a1193bc6a
Momoko Kaneko <golden.sps2014@gmail.com>
2026-07-30 21:34:14 +0900

    [mod]ex0

--- PM02/ex0/ft_first_exception.py
+++ PM02/ex0/ft_first_exception.py
@@ -1,16 +1,22 @@
+from typing import Any
+
 def input_temparature(temp_str: str) -> int:
     temp_int = int(temp_str)
     return temp_int
 
 
+def verify_and_print_temparature(val: Any) -> None:
+    try:
+        print(f"Input data is '{val}'")
+        temp_valid = input_temparature(val)
+        print(f"Temparature is now {temp_valid}°C")
+    except(ValueError, TypeError) as e:
+        print(f"Caught input_temparature error: {e}")
+
+
 def test_temparature() -> None:
-    for vals in ["25", "abc", None]:
-        try:
-            print(f"Input data is '{vals}'")
-            temp_valid = input_temparature(vals)
-            print(f"Temparature is now {temp_valid}°C")
-        except (ValueError, TypeError) as e:
-            print(f"Caught input_temparature error: {e}")
+    verify_and_print_temparature("25")
+    verify_and_print_temparature("abc")
 
 
 def main() -> None:
