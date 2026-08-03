97fa60adb5be6ec5544151cff05e044371535bd4
Momoko Kaneko <mkaneko@c6r4s6.42tokyo.jp>
2026-08-03 17:55:47 +0900

    [mod]ex6

--- PM03/ex6/ft_data_alchemist0801.py
+++ PM03/ex6/ft_data_alchemist0801.py
@@ -6,30 +6,20 @@ def main() -> None:
 
     capitalized_players: list[str] = [name.capitalize() for name in players]
 
-    capitalized_only: list[str] = [
-        name for name in players if name[0].isupper()
-    ]
+    capitalized_only: list[str] = [name for name in players if name[0].isupper()]
 
-    scores: dict[str, int] = {
-        name: random.randint(50, 100) for name in capitalized_players
-    }
+    scores: dict[str, int] = {name: random.randint(50, 100) for name in capitalized_players}
 
-    total_players = len(scores)
-    average_score: float = (
-        sum(scores.values()) / total_players if total_players > 0 else 0.0
-    )
+    average_score: float = sum(scores.values()) / len(scores)
     high_scores: dict[str, int] = {
-        name: score
-        for name, score in scores.items()
-        if score > average_score
-    }
+        name: score for name, score in scores.items() if score > average_score}
 
     print("=== Game Data Alchemist ===\n")
     print("Initial list of players: ", players)
     print("New list with all names capitalized: ", capitalized_players)
     print("New list of capitalized names only: ", capitalized_only)
     print("Score dict:", scores)
-    print(f"Score average is {average_score:.1f}")
+    print("Score average is ", average_score)
     print("High scores: ", high_scores)
 
 
