536bdcd9e42e9e2c4bfff84b00ace1ad4c5fd1c8
Momoko Kaneko <golden.sps2014@gmail.com>
2026-08-06 23:10:27 +0900

    [mod]ex0 add return

--- PM04/ex0/ft_ancient_text.py
+++ PM04/ex0/ft_ancient_text.py
@@ -1,19 +1,7 @@
+#! /usr/bin/env python3
+
 import sys
 import typing
-
-# def get_file_name(args_lst: list[str]) -> str:
-#     if len(args_lst) < 2:
-#         print("Usage: ft_ancient_text.py <file>")
-#         return None
-
-#     try:
-#         file_name: str = args_lst[1]
-#         print(f"Accessing file '{file_name}'")
-#         return file_name
-
-#     except (ValueError, TypeError, IndexError) as e:
-#         print(f"Usage: {args_lst[0]} <file> {e}")
-#         return None
 
 
 def main() -> None:
@@ -25,21 +13,26 @@ def main() -> None:
     print("=== Cyber Archives Recovery ===")
     print(f"Accessing file '{file_name}'")
 
+    f: typing.IO[str] | None = None
+
     try:
         f: typing.IO[str] = open(file_name, "r")
-    except(FileNotFoundError, PermissionError, IsADirectoryError) as e:
+        content: str = f.read()
+        print("---\n")
+        print(content)
+        print("\n---")
+    except (
+        FileNotFoundError,
+        PermissionError,
+        IsADirectoryError,
+        UnicodeDecodeError
+    ) as e:
         print(f"Error opening file {file_name}: {e}")
         return
-
-    content: str = f.read()
-
-    print("---\n")
-    print(content)
-    print("\n---")
-
-    f.close()
-    print(f"File '{file_name}' closed.")
-
+    finally:
+        if f is not None:
+            f.close()
+            print(f"File '{file_name}' closed.")
 
 if __name__ == "__main__":
     main()
