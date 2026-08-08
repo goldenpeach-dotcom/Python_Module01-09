9fa71cf2b7537826e0a407f6b4b4dd97437db2dd
Momoko Kaneko <golden.sps2014@gmail.com>
2026-08-08 21:13:49 +0900

    [add]ex3

--- PM04/ex0/ft_ancient_text.py
+++ PM04/ex0/ft_ancient_text.py
@@ -16,7 +16,7 @@ def main() -> None:
     f: typing.IO[str] | None = None
 
     try:
-        f: typing.IO[str] = open(file_name, "r")
+        f = open(file_name, "r")
         content: str = f.read()
         print("---\n")
         print(content)
--- PM04/ex3/ft_vault_security.py
+++ PM04/ex3/ft_vault_security.py
@@ -1,6 +1,6 @@
 def secure_archive(
     file_name: str,
-    operation: str,
+    operation: str = "r",
     content: str = ""
 ) -> tuple[bool, str]:
     try:
@@ -11,8 +11,32 @@ def secure_archive(
         elif operation == "w":
             with open(file_name, "w") as f:
                 f.write(content)
-            return (True, str(content))
+            return (True, "Content successfully written to file")
         else:
-            return(False,)
+            return (False, "Mode error")
     except OSError as e:
         return (False, str(e))
+
+
+def main() -> None:
+    print("=== Cyber Archives Security ===")
+    print("Using 'secure_archive' to read from a nonexistent file:")
+    res: tuple[bool, str] = secure_archive("/not/existing/file", "r", "")
+    print(res)
+
+    print("Using 'secure_archive' to read from an inaccessible file:")
+    res: tuple[bool, str] = secure_archive("etc/master.passwd", "r", "")
+    print(res)
+
+    print("Using 'secure_archive' to read from a regular file:")
+    res: tuple[bool, str] = secure_archive("ancient_fragment.txt", "r", "")
+    print(res)
+
+    print("Using 'secure_archive' to write previous content to a new file:")
+    res: tuple[bool, str] = secure_archive(
+        "test.txt", "w", "aaaaaaaaaaaaaaaaaaa"
+        )
+    print(res)
+
+if __name__ == "__main__":
+    main()
