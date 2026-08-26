2b34bd0a7f755a2021d8590d675495e6829abeb8
Momoko Kaneko <mkaneko@c1r7s5.42tokyo.jp>
2026-08-26 19:16:38 +0900

    [mod]alchemy/__init__.py & transmutation_2

--- PM06/alchemy/__init__.py
+++ PM06/alchemy/__init__.py
@@ -1,9 +1,10 @@
 from .elements import create_air
 from .potions import strength_potion
 from .potions import healing_potion as heal
-from .import transmutation
+from . import transmutation
+from .transmutation import lead_to_gold
 
-__all__ = ["create_air", "strength_potion", "heal", "transmutation"]
+__all__ = ["create_air", "strength_potion", "heal", "transmutation", "lead_to_gold"]
 
 
 # __all__公開インターフェース　ここに定義されたものは公開される。
--- PM06/ft_transmutation_2.py
+++ PM06/ft_transmutation_2.py
@@ -6,7 +6,7 @@ import alchemy
 def main() -> None:
     print("=== Transmutation 2 ===")
     print("Import alchemy module only")
-    print(f"Testing lead to gold: {alchemy.transmutation.lead_to_gold()}")
+    print(f"Testing lead to gold: {alchemy.lead_to_gold()}")
 
 
 if __name__ == "__main__":
