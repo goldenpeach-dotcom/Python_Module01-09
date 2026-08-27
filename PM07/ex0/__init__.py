1e44c63c2882c3b05e84d8198d47cfc389af8a70
Momoko Kaneko <golden.sps2014@gmail.com>
2026-08-27 16:12:15 +0900

    [mod]ex1/__init__.py

---
+++ PM07/ex0/__init__.py
@@ -0,0 +1,9 @@
+from .factories.creature_factory import CreatureFactory
+from .factories.flame_factory import FlameFactory
+from .factories.aqua_factory import AquaFactory
+
+__all__ = [
+    "CreatureFactory",
+    "FlameFactory",
+    "AquaFactory",
+]
--- PM07/ex1/__init__.py
+++ PM07/ex1/__init__.py
@@ -1,10 +1,7 @@
-from ex0.factories.creature_factory import CreatureFactory
 from .factories import HealingCreatureFactory, TransformCreatureFactory
 
 
 __all__ = [
-    "FlameFactory",
-    "AquaFactory",
     "HealingCreatureFactory",
     "TransformCreatureFactory"
 ]
